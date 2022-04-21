4e5ef852b45e2b210b3ca2f6370f638e6c4f8dae
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2022-04-21 14:54:38 +0100

    Completed plugin

---
+++ Koha/Plugin/Com/PTFSEurope/Crossref/Api.pm
@@ -0,0 +1,77 @@
+package Koha::Plugin::Com::PTFSEurope::Crossref::Api;
+
+use Modern::Perl;
+use strict;
+use warnings;
+
+use File::Basename qw( dirname );
+use JSON qw( decode_json );
+use LWP::UserAgent;
+
+use Koha::Logger;
+use Koha::Patrons;
+use Mojo::Base 'Mojolicious::Controller';
+use Koha::Plugin::Com::PTFSEurope::Crossref;
+
+sub works {
+    # Validate what we've received
+    my $c = shift->openapi->valid_input or return;
+
+    my $base_url = "https://api.crossref.org/works/";
+
+    # Check we've got an email address to append to the API call
+    # Error if not
+    my $email = _get_email();
+    $email =~ s/^\s+|\s+$//g;
+    if (!$email || length $email == 0) {
+        _return_response({ error => 'No email address configured' }, $c);
+    }
+    my $doi = $c->validation->param('doi') || '';
+
+    if (!$doi || length $doi == 0) {
+        _return_response({ error => 'No DOI supplied' }, $c);
+    }
+
+    my $ua = LWP::UserAgent->new;
+    my $response = $ua->get("${base_url}${doi}?mailto=$email");
+
+    use Data::Dumper;
+    print STDERR Dumper $response;
+
+    if ( $response->is_success ) {
+        print STDERR $response->decoded_content;
+        _return_response({ success => decode_json($response->decoded_content) }, $c);
+    } else {
+        _return_response(
+            {
+                error => $response->status_line,
+                errorcode => $response->code
+            },
+            $c
+        );
+
+    }
+}
+
+sub _return_response {
+    my ( $response, $c ) = @_;
+    return $c->render(
+        status => $response->{errorcode} || 200,
+        openapi => {
+            results => {
+                result => $response->{success} || [],
+                errors => $response->{error} || []
+            }
+        }
+    );
+}
+
+sub _get_email {
+
+    my $plugin = Koha::Plugin::Com::PTFSEurope::Crossref->new();
+    my $config = decode_json($plugin->retrieve_data("crossref_config") || {});
+
+    return $config->{crossref_email_address};
+}
+
+1;
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # koha-plugin-api-crossref
-A plugin for Koha that allows you to make queries to the CrossRef API via a locally provided JSON API
+This plugin provides a Koha API route enabling access to the /Works Crossref API endpoint
+
+## Getting Started
+
+Download this plugin by downloading the latest release .kpz file from the [releases page](https://github.com/PTFS-Europe/koha-plugin-api-crossref/releases).
+
+The plugin system needs to be turned on by a system administrator.
+
+To set up the Koha plugin system you must first make some changes to your install.
+
+Change `<enable_plugins>0<enable_plugins>` to `<enable_plugins>1</enable_plugins>` in your `koha-conf.xml` file
+Confirm that the path to `<pluginsdir>` exists, is correct, and is writable by the web server.
+Restart your webserver.
+Once set up is complete you will need to alter your `UseKohaPlugins` system preference.
+Finally, on the "Koha Administration" page you will see the "Manage Plugins" option, select this to access the Plugins page.
+
+### Installing
+
+Once your Koha has plugins turned on, as detailed above, installing the plugin is then a case of selecting the "Upload plugin" 
+button on the Plugins page and navigating to the .kpz file you downloaded
+
+### Configuration
+
+**The plugin requires configuration prior to usage**. To configure the plugin, select the "Actions" button listed by the plugin in the "Plugins" page, then select "Configure". On the configure page, you are required to supply an email address, then click "Save configuration"
+
+
+
+## Authors
+
+* Andrew Isherwood
